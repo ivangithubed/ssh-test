a4d42bc9db0ae12cfb6eb14f7aad46812a32f30e
ivangithubed <fmdostoevskiy@gmail.com>
2025-06-09 01:01:03 +0300

    fix json-2, delete .esignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,15 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/package-lock.json",
+      "**/yarn.lock"
+    ]
+  },
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
